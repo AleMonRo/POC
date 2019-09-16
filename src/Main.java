c959ae525fb6785b98fe6bd733261c4d89afcd02
Alejandra Montaño Rodrigo <Alejandra Montano@jalasoft.local>
2019-09-15 20:00:15 -0400

    Refactor and adding styles

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,38 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main<Board> extends Application {
+
+    private static final String BLANK_STRING = " ";
+
+    private List<HBox> hBoxes = new ArrayList<>();
+    private List<Label> allLabels = new ArrayList<>();
+    private int gridSize = 3;
+    private double spacingSize = 15;
+    private double tileSize = 100;
 
     @Override
     public void start(Stage stage) throws Exception{
         stage.setTitle("2048");
-        //stage.setWidth(400);
-        //stage.setHeight(500);
 
         VBox root = new VBox();
-        root.setSpacing(20);
-        HBox c1 = new HBox();
-        c1.setSpacing(25);
-        HBox c2 = new HBox();
-        c2.setSpacing(25);
-        HBox c3 = new HBox();
-        c3.setSpacing(25);
-
-        Label t1 = new Label("A");
-        Label t2 = new Label("A");
-        Label t3 = new Label("A");
-        c1.getChildren().addAll(t1, t2, t3);
-        Label t4 = new Label("A");
-        //t4.setText("65");
-        //t4
-        Label t5 = new Label("A");
-        Label t6 = new Label("A");
-        c2.getChildren().addAll(t4, t5, t6);
-        Label t7 = new Label("A");
-        Label t8 = new Label("A");
-        Label t9 = new Label("A");
-        c3.getChildren().addAll(t7, t8, t9);
-
-        root.getChildren().addAll(c1, c2, c3);
+        root.setSpacing(spacingSize);
+        root.setPadding(new Insets(spacingSize));
+        HBox hbox;
+        for(int i = 0; i < gridSize; i++) {
+            hbox = getNewHboxWithTiles();
+            hbox.setSpacing(spacingSize);
+            hBoxes.add(hbox);
+        }
+        root.getChildren().addAll(hBoxes);
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add("board.css");
@@ -49,10 +40,22 @@ public class Main<Board> extends Application {
         stage.show();
     }
 
+    private HBox getNewHboxWithTiles() {
+        HBox row = new HBox();
+        Label label;
+        List<Label> labels = new ArrayList<>();
+        for(int i = 0; i < gridSize; i++){
+            label = new Label(BLANK_STRING);
+            label.setPrefSize(tileSize, tileSize);
+            labels.add(label);
+        }
+        row.getChildren().addAll(labels);
+        allLabels.addAll(labels);
+        return row;
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 
 }
